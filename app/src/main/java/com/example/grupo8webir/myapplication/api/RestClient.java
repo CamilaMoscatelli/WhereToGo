dc99092c8e8b859177429c7719bd5c1bda7ba7cf
credential.helper <camila.moscatelli@fing.edu.uy>
2016-10-23 00:59:14 -0300

    Api added

---
+++ app/src/main/java/com/example/grupo8webir/myapplication/api/RestClient.java
@@ -0,0 +1,33 @@
+package com.example.grupo8webir.myapplication.api;
+
+import com.loopj.android.http.*;
+
+/**
+ * Created by Camila on 23/10/2016.
+ */
+public class RestClient {
+
+    private static final String BASE_URL = "https://api.where2go.com/v1/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    private static final int TIMEOUT = 45000; // 45 seconds
+    private static final int RETRIES = 0;
+
+    public RestClient() {
+        client.setTimeout(TIMEOUT);
+        client.setMaxRetriesAndTimeout(RETRIES, TIMEOUT);
+    }
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler); //TODO probably not used
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
