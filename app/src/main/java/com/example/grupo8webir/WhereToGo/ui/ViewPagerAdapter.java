2d3b5a2b9831a4642ab29d45d9cceb669b6dbbf8
credential.helper <camila.moscatelli@fing.edu.uy>
2016-11-13 14:05:25 -0300

    Results tabs added

---
+++ app/src/main/java/com/example/grupo8webir/WhereToGo/ui/ViewPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.grupo8webir.WhereToGo.ui;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Camila on 13/11/2016.
+ */
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new TabFragment();    // Which Fragment should be dislpayed by the viewpager for the given position
+        // In my case we are showing up only one fragment in all the three tabs so we are
+        // not worrying about the position and just returning the TabFragment
+    }
+
+    @Override
+    public int getCount() {
+        return 3;           // As there are only 3 Tabs
+    }
+
+}
