b82a1c7ac724b83e3bc4f7714b454e892ea950d8
Rodrigo Eizmendi <rodrigo.eizmendi@fing.edu.uy>
2016-11-22 21:20:10 -0300

    Adds a full screen

--- app/src/main/java/com/example/grupo8webir/WhereToGo/ui/dPeliculas.java
+++ app/src/main/java/com/example/grupo8webir/WhereToGo/ui/dPeliculas.java
@@ -3,16 +3,25 @@ package com.example.grupo8webir.WhereToGo.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.grupo8webir.WhereToGo.R;
 
 public class dPeliculas extends AppCompatActivity {
     Double lat;
     Double lon;
+    //Datos del evento
+    Integer mins = 0;
+    String gen = "Algo";
+    String titulo = "Titulo de la pelicula";
+    String synopsis = "En 1926 New Scamander se prepara para iniciar una excursión por todo el mundo en busca de una extraordinaria variedad de criaturas mágicas. Al llegar a Nueva York, para hacer una breve escala, todo parece ir bien, hasta que un muggle llamado Jacob y la fuga de algunas bestias fantásticas de Newt, podrian representar un verdadero problema tanto para la comunidad mágica como para los muggles.";
+    String duracion = "Duracion: " + mins + " minutos";
+    String genero = "Genero: " + gen;
     //Datos de los shows, el primero se muestra por defecto
     String [] datosShows = {"Shows","show 1","show 2", "show 2"};
     @Override
@@ -20,12 +29,27 @@ public class dPeliculas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_d_peliculas);
 
+        // Imagen del evento
         ImageView iv = (ImageView)  findViewById(R.id.pImagen);
-        iv.setImageResource(R.drawable.animalesfantasticos);
+        iv.setImageResource(R.drawable.animalesfantasticos); //imagen!
+        // Titulo
+        TextView tvTitulo = (TextView) findViewById(R.id.pTitulo);
+        tvTitulo.setText(titulo);
+        // Shows
         Spinner shows = (Spinner) findViewById(R.id.spinnerShows);
-
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,datosShows);
         shows.setAdapter(adaptador);
+        // Synopsis
+        TextView tvSynopsis= (TextView) findViewById(R.id.pSynopsis);
+        tvSynopsis.setText(synopsis);
+        tvSynopsis.setMovementMethod(new ScrollingMovementMethod());
+        // Duracion
+        TextView tvDuracion = (TextView) findViewById(R.id.pDuracion);
+        tvDuracion.setText(duracion);
+        // Genero
+        TextView tvGenero = (TextView) findViewById(R.id.pGenero);
+        tvGenero.setText(genero);
+
 
     }
 
